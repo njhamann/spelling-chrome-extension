2e8c9ea03a50b734065c6fd48cc50a8a14f771dd
Noah Hamann <njhamann@gmail.com>
2013-01-11 10:54:23 -0500

    cleanup and moving icons

--- spelling.js
+++ spelling.js
@@ -1,10 +1,14 @@
 var Spelling = function(){
 	var text = {};
-
+	text.oldText = '';
+	text.newText = '';
+	
 	var checkSpelling = function(str, callback){
-		
+		var url = 'http://public.noahhamann.com/spelling/index.php/api/terms/check';
+		var data = 'copy='+str;
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://public.noahhamann.com/spelling/index.php/api/terms/check?copy="+str, true);
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded")
 		xhr.onreadystatechange = function() {
 		    if (xhr.readyState == 4) {
 		    	var obj = JSON.parse(xhr.responseText);
@@ -12,47 +16,46 @@ var Spelling = function(){
 		    		callback(obj);
 		  	}
 		};
-		xhr.send();
+		xhr.send(data);
 	};
 
-	var events = function(){
-		var text = {};
+	var getExistingText = function(e){
+		console.log('text focus');
+		var target = e.target;
+		var type = $(target).attr('type');
+		if(type == 'password') return;
+		text.oldText = target.isContentEditable ? $(target).text() : $(target).val();
+	};
+
+	var getCurrentText = function(e){
+		console.log('text blur');
+		var target = e.target;
+		var type = $(target).attr('type');
+		if(type == 'password') return;
+		text.newText = target.isContentEditable ? $(target).text() : $(target).val();
+		text.newText = $.trim(text.newText);
+		text.oldText = $.trim(text.oldText);
+
+		var addedWords = diffString(text.oldText, text.newText);
+		var val = addedWords.join();
+		if(addedWords.length > 0){
+			var val = addedWords.join();
+			checkSpelling(val, function(data){
+				console.log(data);
+			});
+		}
 		text.oldText = '';
 		text.newText = '';
-
-		$('input, textarea, [contenteditable=true]').focus(function(){
-			console.log('text focus');
-			text.oldText = this.isContentEditable ? $(this).text() : $(this).val();
-
-		});
-
-		$('input, textarea, [contenteditable=true]').blur(function(){
-			text.newText = this.isContentEditable ? $(this).text() : $(this).val();
-			text.newText = $.trim(text.newText);
-			text.oldText = $.trim(text.oldText);
-
-			var addedWords = diffString(text.oldText, text.newText);
-			var val = addedWords.join();
-			if(addedWords.length > 0){
-				var val = addedWords.join();
-				checkSpelling(val, function(data){
-					console.log(data);
-				});
-			}
-			text.oldText = '';
-			text.newText = '';
-		});
 	};
+
+	//events
+	$('input, textarea, [contenteditable=true]').focus(getExistingText);
+	$('input, textarea, [contenteditable=true]').blur(getCurrentText);
+
+
 	return{
-		checkSpelling: checkSpelling,
-		events: events
-	}
+		checkSpelling: checkSpelling
+	};
 };
 
-var addedWords = diffString(
-   "The red brown fox jumped over the rolling log.",
-   "The brown spotted fox leaped over the rolling log"
-);
-
 var spell = new Spelling();
-spell.events();
